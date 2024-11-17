8a64f5e3328e6060b79f9a5327938b292bce7d93
Osama <xekkon6@gmal.com>
2024-11-17 22:47:11 +0200

    Task1

---
+++ ex1/ex1.js
@@ -0,0 +1,5 @@
+'use strict';
+console.log("I'm printing to console!")
+const answer = prompt("Title", "initial content for the text field")
+//printing the response to the console
+console.log(answer);
